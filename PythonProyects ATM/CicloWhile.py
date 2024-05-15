0695f99c8b5c32012e4ca6251c88ce6fb5d1b662
Hystoreek <147000899+ATM2008@users.noreply.github.com>
2024-05-15 17:58:53 -0500

    Add files via upload

---
+++ PythonProyects ATM/CicloWhile.py
@@ -0,0 +1,35 @@
+#Imprimir un saludo 10 veces
+#Esto lo llamamos contador
+
+saludo = "Hola mundo"
+
+veces = 10
+
+i = 0
+
+while i < veces:
+    print(saludo)
+    i+=1
+
+numero = 0
+
+while numero <= 1000000:
+    print(numero)
+    numero+=100
+
+#acumuladores
+
+sum = 0
+
+veces = 10
+
+num = 0
+
+while num < veces:
+    #sum = sum + num
+
+    sum += num
+
+    print(sum)
+
+    num+=1
